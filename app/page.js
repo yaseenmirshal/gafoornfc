2801071d6f7d76e4af40efa9c2f6d0cf9e001ab8
yaseenmirshal <151372491+yaseenmirshal@users.noreply.github.com>
2025-01-14 14:15:18 +0530

    profile updated

--- app/page.js
+++ app/page.js
@@ -64,7 +64,7 @@ export default function Home() {
           data-aos="zoom-in"
         >
           <Image
-            src="/gafoornew.jpeg"
+            src="/gafoorcrop.png"
             alt="Profile"
             width={144}
             height={144}
